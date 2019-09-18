6052f823b626271db8069d022a6dc4f4103c0780
samuel.lefevr@gmail.com <samu@Mac-mini-de-Samuel.local>
2019-09-18 04:00:05 +0200

    add handleAuth

---
+++ lib/controllers/log_controller.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LogController extends StatefulWidget {
+  _LogState createState() => _LogState();
+}
+
+class _LogState extends State<LogController> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Log"));
+  }
+}
---
+++ lib/controllers/main_app_controller.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MainAppController extends StatefulWidget {
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainAppController> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Main"));
+  }
+}
